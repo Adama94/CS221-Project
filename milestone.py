3ddf40f1b2401d5d5971ece8234521e00194a0b9
awarmoth <awarmoth@stanford.edu>
2015-11-10 17:12:10 -0800

    Update milestone.py

--- milestone.py
+++ milestone.py
@@ -492,13 +492,30 @@ csp = createCSPWithVariables()
 salaryCap = 60000
 addConstraints(csp, salaryCap)
 
-
 '''
+import csv,collections
+
+with open('2015W9.txt') as inputfile:
+	results = list(csv.reader(inputfile))
+salaries = {}
+for i in range(1,len(results)):
+	line = results[i]
+	if line[4] == 'Def':
+		continue
+	firstName = line[4]
+	lastName = line[3]
+	salary = line[10]
+	salaries['%s %s' %(firstName,lastName)] = salary
+print salaries['Andrew Luck']
+
+
 #### TODO ####
 
 Modify createCSPWithVariables so that variable values are (Player, cost) tuples
 Modify get_sum_variable to use this new value of the variables to ensure cost is the variable being summed
 Modify addConstraints so that the constraint is set appropriately
+
+
 
 get_sum_variable doesn't finish now, after updates it shouldn't be too slow
 '''
